dc30653f53e911e1ec5494485e956f9d762c3903
Ponnarasi <ponnarasi.a@techconative.com>
2023-07-27 20:40:22 +0530

    removed oas31 folder

--- src/core/components/layouts/base.jsx
+++ src/core/components/layouts/base.jsx
@@ -33,7 +33,9 @@ export default class BaseLayout extends React.Component {
             </Col>
           </Row>
           <Increment />
-          <div>1st Commit ! 20:35</div>
+          <div>1st Commit : 20:35</div>
+          <div>2st Commit : 20:38</div>
+
       </div>
     )
   }
